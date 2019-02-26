bcbf537ad968232b01b63810ba6fe10bf8371d9e
chenyue <chenyue@leinao.ai>
2019-02-26 19:17:27 +0800

    api地址

--- src/common/api/api.js
+++ src/common/api/api.js
@@ -1,3 +1,1 @@
-// export const APIYRL = 'http://192.168.1.69:8080'
-// export const APIYRL = 'http://139.129.203.44:8380'
-export const APIYRL = 'http://139.129.203.44:8680'
+export const APIYRL = 'http://www.nxptdn.com/backup'
